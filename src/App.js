0884cb6c0b2d92e5df44dc00b3ec6799dc71b798
nomeo-310 <onomesalomi@gmail.com>
2023-02-25 08:02:45 +0100

    the start of the project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+import './css/all.min.css';
+import './css/brands.css';
+import './css/brands.min.css';
+import './css/fontawesome.min.css';
+import './material_icons/css/materialdesignicons.css';
+
+
+const App =() => {
+  
+  return (
+    <div className="app">
+        App
+    </div>
+  );
+}
+
+export default App;
